4ca628377cf9a9ba67c8428d5a09ae1ef5d90d9f
Leandro Abreu <leandro.sabreu@uvvnet.com.bru>
2025-02-05 18:00:54 -0300

    feat: Implementing Test Project

---
+++ HealthMed.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace HealthMed.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            bool result = true;
+            Assert.True(result);
+        }
+    }
+}
