9ac1f9d0ea2fe3f72b343ed885f16a6edd4984ac
Leandro Abreu <leandro.sabreu@uvvnet.com.bru>
2025-02-06 19:27:30 -0300

    fix: Local HTTPS

--- HealthMed.Presentation/Program.cs
+++ HealthMed.Presentation/Program.cs
@@ -2,7 +2,6 @@ using HealthMed.Presentation;
 
 public class Program
 {
-    private static string Ambiente { get; set; }
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
@@ -12,7 +11,6 @@ public class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                Ambiente = context.HostingEnvironment.EnvironmentName;
                 config.SetBasePath(Directory.GetCurrentDirectory());
                 config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
                 config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
@@ -26,19 +24,19 @@ public class Program
             })
             .ConfigureWebHostDefaults(webBuilder =>
             {
-                //if (Ambiente != "Production")
-                //{
-                //    webBuilder.ConfigureKestrel(serverOptions =>
-                //    {
-                //        serverOptions.ListenAnyIP(8080);  // HTTP port
-                //        serverOptions.ListenAnyIP(8443, listenOptions =>  // HTTPS port
-                //        {
-                //            var certPath = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path");
-                //            var certPassword = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password");
-                //            listenOptions.UseHttps(certPath!, certPassword);
-                //        });
-                //    });
-                //}
+                if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Production")
+                {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.ListenAnyIP(8080);  // HTTP port
+                        serverOptions.ListenAnyIP(8443, listenOptions =>  // HTTPS port
+                        {
+                            var certPath = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path");
+                            var certPassword = Environment.GetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password");
+                            listenOptions.UseHttps(certPath!, certPassword);
+                        });
+                    });
+                }
 
                 webBuilder.UseStartup<Startup>();
             });
